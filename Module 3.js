8950b5d2faf982d8c2bc8b12f1bf38a5dda72dbd
aaron-sunceri <122327934+aaron-sunceri@users.noreply.github.com>
2023-01-29 22:53:04 -0500

    Add files via upload

---
+++ Module 3.js
@@ -0,0 +1,82 @@
+﻿//---------//
+// Strings //
+//---------//
+console.log("Strings");
+
+// 1. Make all countries in the following array follow the same formatting:
+//    The first letter should be upper case, the rest lower case. Print the
+//    result to the console.
+
+var countries = ["Australia", "Greece", "Peru", "China"];
+console.log(countries);
+
+// 2. Get every second and third letter from each country and concatenate them.
+//    Print the results to the console.
+let text = "Australia";
+let result = text.substring(1, 3);
+let text = "Greece";
+let result = text.substring(1, 3);
+let text = "Peru";
+let result = text substring(1, 3);
+let text = "China";
+let result = text.substring(1, 3);
+// 3. In the sentence below replace 'dart' with 'arrow' by using the indexOf
+//    and slice functions.
+
+var sentence = "Time flies like an dart; fruit flies like a banana.";
+console.log("Time flies like an dart; fruit flies like a banana".replaceAll('darts', 'arrow'));
+
+// 4. Find the index of the last occurrence of flies in sentence.
+indexOf("Time flies like an dart; fruit flies like a banana", "fruit flies like a banana");
+// 5. Prompt the user for a string, reverse it, and show the result to the user.
+
+var five = prompt("Reverse it");
+five.reverse;
+alert(var five);
+
+//---------//
+// Numbers //
+//---------//
+console.log("Numbers");
+
+// Use the following variable to answer the next questions.
+
+var value = 13.3;
+
+// 1. Round the value using normal math rules.
+Math.Round(13.3);
+// 2. Round the value up.
+Math.ceil(13.3);
+// 3. Round the value down.
+Math.Floor(13.3);
+// 4. Simulate a coin flip by generating a random number. Assume the coin is
+//    fair and print the result, heads or tails, to the console.
+def coinFlip(p):
+
+result = random.binomial(1, p)
+return result
+probability = .5
+n = 10
+fullResults = np.arange(n)
+for i in range(0, n):
+    fullResults[i] = coinFlip(probability)
+i += 1
+print("probability is set to ", probability)
+print("Tails = 0, Heads = 1: ", fullResults)
+print("Head Count: ", np.count_nonzero(fullResults == 1))
+print("Tail Count: ", np.count_nonzero(fullResults == 0))
+
+//-------//
+// Dates //
+//-------//
+console.log("Dates");
+
+// 1. Prompt the user for their age and proceed to show in what year they
+//    were born (you can assume they already had their birthday in the current
+//    year).
+
+// 2. Make a program that does the following: (1) Use a prompt to obtain the
+//    birth year of the user. (2) Use a prompt to obtain the birth month of
+//    the user. (3) Use a prompt to obtain the birth day of the user.
+//    (4) Calculate how old the user is in days. (5) Show the result to
+//    the user.
